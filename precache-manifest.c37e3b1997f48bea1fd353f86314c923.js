2dbba923d23e3a8c7a3e3e0643fac90f8f817860
HTMLProgrammer2001 <cssuperpy@gmail.com>
2020-11-02 17:46:34 +0200

    Updates

---
+++ precache-manifest.c37e3b1997f48bea1fd353f86314c923.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b35e0dde1129b482499a5527a2cc003",
+    "url": "/trackTeacherFront/index.html"
+  },
+  {
+    "revision": "66919501b1a324448211",
+    "url": "/trackTeacherFront/static/css/10.33013ea9.chunk.css"
+  },
+  {
+    "revision": "18c7004a76b7c990a7b2",
+    "url": "/trackTeacherFront/static/css/11.48e51f71.chunk.css"
+  },
+  {
+    "revision": "b3162d8917b4e557824f",
+    "url": "/trackTeacherFront/static/css/4.7ee46f2e.chunk.css"
+  },
+  {
+    "revision": "7f5434a0fe2f87958c2b",
+    "url": "/trackTeacherFront/static/css/9.b94fe8fa.chunk.css"
+  },
+  {
+    "revision": "08f342858296e3a7e951",
+    "url": "/trackTeacherFront/static/css/main.6487dbe8.chunk.css"
+  },
+  {
+    "revision": "7548694bea5bc89ff09d",
+    "url": "/trackTeacherFront/static/js/0.10ac9567.chunk.js"
+  },
+  {
+    "revision": "54d9d235c4f8017f87a4",
+    "url": "/trackTeacherFront/static/js/1.3700c8e1.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/trackTeacherFront/static/js/1.3700c8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66919501b1a324448211",
+    "url": "/trackTeacherFront/static/js/10.541354e9.chunk.js"
+  },
+  {
+    "revision": "18c7004a76b7c990a7b2",
+    "url": "/trackTeacherFront/static/js/11.bc3670de.chunk.js"
+  },
+  {
+    "revision": "63da22ba85240eccf0e7",
+    "url": "/trackTeacherFront/static/js/12.5cbe7a29.chunk.js"
+  },
+  {
+    "revision": "91da9562dcf5289cb263",
+    "url": "/trackTeacherFront/static/js/13.af9cba8a.chunk.js"
+  },
+  {
+    "revision": "b3162d8917b4e557824f",
+    "url": "/trackTeacherFront/static/js/4.bf69b9f7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/trackTeacherFront/static/js/4.bf69b9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a9df878268e5e88379",
+    "url": "/trackTeacherFront/static/js/5.5016eb23.chunk.js"
+  },
+  {
+    "revision": "67f06857e6774582b7e9",
+    "url": "/trackTeacherFront/static/js/6.44ced19e.chunk.js"
+  },
+  {
+    "revision": "48283fbad842efa52266",
+    "url": "/trackTeacherFront/static/js/7.b815b42c.chunk.js"
+  },
+  {
+    "revision": "76f0ffcd98ac0d78861f",
+    "url": "/trackTeacherFront/static/js/8.d9d7f46b.chunk.js"
+  },
+  {
+    "revision": "7f5434a0fe2f87958c2b",
+    "url": "/trackTeacherFront/static/js/9.17c1641a.chunk.js"
+  },
+  {
+    "revision": "08f342858296e3a7e951",
+    "url": "/trackTeacherFront/static/js/main.8e603f78.chunk.js"
+  },
+  {
+    "revision": "d4d58c64e0dcbfbd86fd",
+    "url": "/trackTeacherFront/static/js/runtime-main.8dbde864.js"
+  }
+]);
